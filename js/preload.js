1737d05811bb0434336e43caea32d3e2383c3c1e
Georgios Panayiotou <georgios.panayiotou@outlook.com>
2013-04-25 23:11:40 +0200

    Ladescreen umgebaut "Tolito" Ladebalken eingebaut (hübsch!) Hintergrundbilder entfernt (hässlich :/) Kleine Verbesserungen...

--- js/preload.js
+++ js/preload.js
@@ -1,6 +1,6 @@
 $(document).bind('mobileinit', function()
 {
-       // JQM starteinstellungen
+
 });
 
 $(document).ready(function ()
@@ -10,96 +10,76 @@ $(document).ready(function ()
     if (cache != undefined)
     {
         // TODO: anzahl der dateien aus manifest datei extrahieren
-        var num_files_total = 25;
+        var num_files_total = 27;
         var num_files_cached = 0;
-        var $progress_bar = document.createElement('progress');
-        $progress_bar.value = num_files_cached;
-        $progress_bar.max = num_files_total;
-        $progress_bar.id = 'eduphil-preloadbar';
 
-        function add_app_link()
+        var $progress_bar = TolitoProgressBar('progressbar')
+        .setOuterTheme('b')
+        .setInnerTheme('b')
+        .isMini(true)
+        .setMax(num_files_total)
+        .setInterval(50)
+        .setStartFrom(num_files_cached)
+        .showCounter(true)
+        .build();
+
+        $(document).on('complete', '#progressbar', function ()
         {
-            var $start_app_link = document.createElement('a');
-            $start_app_link.href = "app.html";
-            $start_app_link.textContent = "App Starten";
-            $start_app_link.id = "startbutton";
-
-            $('#content').append($start_app_link);
-            $('#startbutton').buttonMarkup({ theme: "a" }); // aus link einen button machen
-        }
+            //$('#progressbar-button').text('Starte App');
+            $('#progressbar-button').parent().find('.ui-btn-inner .ui-btn-text').text('Starte App');
+            $('#progressbar-button').attr('href', "app.html");
+        });
 
         // Überprüfe auf neue Version im Netz
         cache.addEventListener('checking', function()
         {
-            $('#content').empty();
-            var html = '<h2>Überprüfe Version... (Checking)</h2>';
-            $('#content').append(html);
+            $('#status-message').text('Überprüfe Version...');
         });
 
         // Version ist aktuell
         cache.addEventListener('noupdate', function()
         {
-            $('#content').empty();
-            var html = '<h2>Sie verwenden die neueste Version. (noupdate)</h2>';
-            $('#content').append(html);
-            add_app_link();
+            $('#status-message').text('Neueste Version vorhanden.');
+            $progress_bar.setValue(num_files_total);
+            $('#progressbar-button').parent().find('.ui-btn-inner .ui-btn-text').text('Starte App');
+            $('#progressbar-button').attr('href', "app.html");
         });
 
         // Download startet
         cache.addEventListener('downloading', function()
         {
-            $('#content').empty();
-            var html = '<h2>Herunterladen... (downloading)</h2>';
-            $('#content').append(html);
-            $('#content').append($progress_bar);
+            $('#status-message').text('Lade App...');
+            $progress_bar.run();
         });
 
         // listener für progress bar, 1x fire -> +1, dann den balken updaten
         cache.addEventListener('progress', function()
         {
             num_files_cached++;
-            $progress_bar.value = num_files_cached;
+            $progress_bar.setValue(num_files_cached);
         });
 
         // update wurde heruntergeladen, installieren?
         cache.addEventListener('updateready', function()
         {
-            $.mobile.changePage("#eduphil-dialog-update", {transition: "slidedown"});
+            cache.update();
         });
 
         // Update abgeschlossen
         cache.addEventListener('cached', function()
         {
-            $('#content').empty();
-            var html = '<h2>Sie verwenden die neueste Version. (cached)</h2>';
-            $('#content').append(html);
-            add_app_link();
+            $('#status-message').text('Sie verwenden die neueste Version.');
+            $progress_bar.setValue(num_files_total);
         });
 
         // Fehler!
         cache.addEventListener('error', function()
         {
-            $.mobile.changePage("#eduphil-dialog-error", {transition: "slidedown"}); //dialog schliessen
-        });
-
-        // Callbacks für die Dialoge
-        $("#eduphil-dialog-button-yes").bind("click", function (event)
-        {
-            $.mobile.changePage("#eduphil-preload", {transition: "pop"}); //dialog schliessen
-            cache.update(); // neuen cache verwenden
-        });
-
-        $('#eduphil-dialog-button-no').bind("click", function(event)
-        {
-            $.mobile.changePage("#eduphil-preload", {transition: "pop"}); //dialog schliessen
-            $('#content').empty();
-            var html = '<li data-role="list-divider">Starte App</li>';
-            $('#content').append(html);
-            add_app_link();
+            $('#status-message').text("Fehler beim laden.");
         });
     }
     else
     {
-        $('#content').html('<p>Fehler: App Installation scheint deaktiviert zu sein (Application cache error).</p>');
+        $('#status-message').text('Fehler: App Installation scheint deaktiviert zu sein.');
     }
 });
