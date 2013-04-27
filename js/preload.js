3a61e48a67b524d311d3a26691567ac18c612fa4
Georgios Panayiotou <georgios.panayiotou@outlook.com>
2013-04-27 17:56:19 +0200

    Diverse CSS tweaks Bilder ein wenig angepasst Default theme wird verwendet -> overrides in "preload.css" und "app.css"  JavaScript etwas verschlankt.  Es kann ab hier noch viel CSS gelöscht werdem weil wir es nciht brauchen, aber das machen wir am Ende

--- js/preload.js
+++ js/preload.js
@@ -1,85 +1,81 @@
 $(document).bind('mobileinit', function()
 {
-
+    // inits für JQM (wird ausgeführt nachdem JQM geladen wurde)
 });
 
 $(document).ready(function ()
 {
     var cache = window.applicationCache;
+
     // ist application cache API verfügbar?
     if (cache != undefined)
     {
         // TODO: anzahl der dateien aus manifest datei extrahieren
-        var num_files_total = 27;
-        var num_files_cached = 0;
+        var num_files_total = 30; // Anzahl aller Dateien
+        var num_files_cached = 0; // Anzahl Datien die bereits geladen sind
 
         var $progress_bar = TolitoProgressBar('progressbar')
-        .setOuterTheme('b')
-        .setInnerTheme('b')
-        .isMini(true)
-        .setMax(num_files_total)
-        .setInterval(50)
-        .setStartFrom(num_files_cached)
-        .showCounter(true)
-        .build();
+                                                            .setOuterTheme('e')
+                                                            .setInnerTheme('e')
+                                                            .isMini(true)
+                                                            .setMax(num_files_total)
+                                                            .setStartFrom(num_files_cached)
+                                                            .showCounter(true)
+                                                            .build();
 
-        $(document).on('complete', '#progressbar', function ()
+        // Überprüfe auf neue Version auf dem Server: CHECKING
+        cache.addEventListener('checking', function(event)
         {
-            //$('#progressbar-button').text('Starte App');
-            $('#progressbar-button').parent().find('.ui-btn-inner .ui-btn-text').text('Starte App');
-            $('#progressbar-button').attr('href', "app.html");
+            $('#progressbar-button').parent().find('.ui-btn-inner .ui-btn-text').attr('href', "#");
+            $('#progressbar-button').parent().find('.ui-btn-inner .ui-btn-text').text('Überprüfe...');
         });
 
-        // Überprüfe auf neue Version im Netz
-        cache.addEventListener('checking', function()
+        // Version ist aktuell: NOUPDATE
+        cache.addEventListener('noupdate', function(event)
         {
-            $('#status-message').text('Überprüfe Version...');
-        });
-
-        // Version ist aktuell
-        cache.addEventListener('noupdate', function()
-        {
-            $('#status-message').text('Neueste Version vorhanden.');
             $progress_bar.setValue(num_files_total);
+            $('#progressbar-button').parent().find('.ui-btn-inner .ui-btn-text').attr('href', "app.html");
             $('#progressbar-button').parent().find('.ui-btn-inner .ui-btn-text').text('Starte App');
-            $('#progressbar-button').attr('href', "app.html");
         });
 
-        // Download startet
-        cache.addEventListener('downloading', function()
+        // App das erste mal gecached: CACHED
+        cache.addEventListener('cached', function(event)
         {
-            $('#status-message').text('Lade App...');
-            $progress_bar.run();
+            $progress_bar.setValue(num_files_total);
+            $('#progressbar-button').parent().find('.ui-btn-inner .ui-btn-text').attr('href', "app.html");
+            $('#progressbar-button').parent().find('.ui-btn-inner .ui-btn-text').text('Starte App');
         });
 
-        // listener für progress bar, 1x fire -> +1, dann den balken updaten
-        cache.addEventListener('progress', function()
+        // Download startet: DOWNLOADING
+        cache.addEventListener('downloading', function(event)
+        {
+            $('#progressbar-button').parent().find('.ui-btn-inner .ui-btn-text').attr('href', "#");
+            $('#progressbar-button').parent().find('.ui-btn-inner .ui-btn-text').text('Lade App...');
+        });
+
+        // listener für progress bar, 1x fire -> +1, dann den balken updaten: PROGRESS
+        cache.addEventListener('progress', function(event)
         {
             num_files_cached++;
             $progress_bar.setValue(num_files_cached);
         });
 
-        // update wurde heruntergeladen, installieren?
-        cache.addEventListener('updateready', function()
+        // update wurde heruntergeladen, verwenden?: UPDATEREADY
+        // TODO: Evtl. nur ein silent update durchführen, ohne popup. Führt aber dazu, dass das App "immer komisch neu lädt"
+        cache.addEventListener('updateready', function(event)
         {
-            cache.update();
+            cache.swapCache(); // neuen cache benutzen
+            $.mobile.changePage("refresh.html", { transition: "slidedown" }); // Seite neu laden
         });
 
-        // Update abgeschlossen
-        cache.addEventListener('cached', function()
+        // Fehler: ERROR
+        cache.addEventListener('error', function(event)
         {
-            $('#status-message').text('Sie verwenden die neueste Version.');
-            $progress_bar.setValue(num_files_total);
-        });
-
-        // Fehler!
-        cache.addEventListener('error', function()
-        {
-            $('#status-message').text("Fehler beim laden.");
+            $.mobile.changePage("error-preload.html", { transition: "slidedown" });
         });
     }
     else
     {
-        $('#status-message').text('Fehler: App Installation scheint deaktiviert zu sein.');
+        $.mobile.changePage("error-nocache.html", { transition: "slidedown" });
     }
 });
