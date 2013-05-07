cd495a1a687c9b1845f71477d2bc0f1ac3bb488f
Georgios Panayiotou <georgios.panayiotou@outlook.com>
2013-05-07 18:50:01 +0200

    Howler JS zum Abspielen von Sound hinzugefügt (testweise) Klasse Musician für die Musiker erstellt (Sound+Anim) Zusätzliche "Hitbox" für die Musiker hinzugefügt, damit überlappen möglich wird Icons und Splash an die CI angepasst Versuch eines "rotationslock" (konnte es nicht testen) links gefixt (?) in den fehlerdialogen

--- js/main.js
+++ js/main.js
@@ -3,61 +3,131 @@ $(document).on('mobileinit', function()
     // inits für JQM
 });
 
-$(document).on('pageload', function(event)
+/*
+ pagebeforeload, pageload and pageloadfailed are fired when an external page is loaded
+ pagebeforechange, pagechange and pagechangefailed are page change events. These events are fired when a user is navigating between pages in the applications.
+ pagebeforeshow, pagebeforehide, pageshow and pagehide are page transition events. These events are fired before, during and after a transition and are named.
+ pagebeforecreate, pagecreate and pageinit are for page initialization.
+ pageremove can be fired and then handled when a page is removed from the DOM
+*/
+
+// Code wird beim laden der #app-page ausgeführt (1x)
+$(document).on('pageinit', '#app-page', function()
 {
-    // Code wird ausgeführt für alle pages, haben aber nur eine, die App-Seite
-
     var num_frames = 12;
+    var normal_speed = 12;
+    var high_speed = 24;
+    var low_speed = 6;
 
-    // Sprites beim start anzeigen: IDLE
-    $('#geigerin').sprite({ fps: 12, no_of_frames: num_frames });
-    $('#knight2').sprite({ fps: 12, no_of_frames: num_frames });
-    $('#knight3').sprite({ fps: 12, no_of_frames: num_frames });
-    $('#knight4').sprite({ fps: 12, no_of_frames: num_frames });
-    $('#knight5').sprite({ fps: 12, no_of_frames: num_frames });
+    // id: CSS id des sprite (mit #)
+    function Musician(id, soundfile_mp3, soundfile_ogg)
+    {
+        this.id = id;
+        this.soundfile_mp3 = soundfile_mp3;
+        this.soundfile_ogg = soundfile_ogg;
+        this.is_playing = false;
+        this.speed = normal_speed;
+        this.sound = new Howl(
+        {
+            urls: [this.soundfile_mp3, this.soundfile_ogg]
+        });
 
+        /*
+            startet Animation
+            startet sound
+        */
+        this.start_playing = function()
+        {
+            $(id).sprite({ fps: this.speed, no_of_frames: num_frames });
+            this.sound.play();
+            this.is_playing = true;
+        }
 
-    var is_destroyed = false;
+        /*
+            stoppt animation
+            stoppt sound
+        */
+        this.stop_playing = function()
+        {
+            $(id).destroy();
+            this.sound.stop();
+            this.is_playing = false;
+        }
+
+        /*
+            setzt neue abspielgeschwindigkeit (nur animation)
+         */
+        this.change_speed = function(speed)
+        {
+            this.speed = speed;
+            $(id).fps(this.speed);
+        }
+    }
+
+    var geigerin = new Musician('#geigerin', 'sound/violin.mp3','sound/violin.ogg');
 
     // Callbacks
     function tap_handler(event)
     {
-        if(is_destroyed)
+        event.stopPropagation(); // event bubbling stoppen
+
+        var musician = {};
+        switch ( $(this).parent()[0].id )
         {
-            $(event.target).sprite({ fps: 12, no_of_frames: num_frames });
-            is_destroyed = false;
+            case 'geigerin': musician = geigerin; break;
+            default: break;
+        }
+
+        if(musician.is_playing)
+        {
+            musician.stop_playing();
         }
         else
         {
-            $(event.target).destroy();
-            is_destroyed = true;
+            musician.start_playing();
         }
     }
 
     function swipeleft_handler(event)
     {
-        $(event.target).fps(6);
+        event.stopPropagation(); // event bubbling stoppen
+        // TODO: grosse switches hier gut?!?
+
+        switch($(this).parent()[0].id)
+        {
+            case 'geigerin': geigerin.change_speed(low_speed); break;
+            default: break;
+        }
     }
 
     function swiperight_handler(event)
     {
-        $(event.target).fps(24);
+        event.stopPropagation(); // event bubbling stoppen
+
+        switch ($(this).parent()[0].id)
+        {
+            case 'geigerin': geigerin.change_speed(high_speed); break;
+            default: break;
+        }
     }
+
     // Bindings
-    $('.sprite').on('tap', tap_handler);
-    $('.sprite').on('swipeleft', swipeleft_handler);
-    $('.sprite').on('swiperight', swiperight_handler);
+    $('.hitbox').on('tap', tap_handler);
+    $('.hitbox').on('swipeleft', swipeleft_handler);
+    $('.hitbox').on('swiperight', swiperight_handler);
 });
 
+
+// Nur für die erste Seite relevant!
 $(document).ready(function ()
 {
-    var cache = window.applicationCache;
-
     // ist application cache API verfügbar?
-    if (cache != undefined)
+    if (window.applicationCache != undefined)
     {
+        var cache = window.applicationCache;
+
         // TODO: anzahl der dateien aus manifest datei extrahieren
-        var num_files_total = 32; // Anzahl aller Dateien
+        var num_files_total = 35; // Anzahl aller Dateien
         var num_files_cached = 0; // Anzahl Datien die bereits geladen sind
 
         var $progress_bar = TolitoProgressBar('progressbar')
